158418948bd6ef6b92613c4dd24dfed91147da3a
Omar ElMaria <omar.elmaria@deliveryhero.com>
2022-10-23 10:27:58 +0200

    add randomize_download_delay=False to custom settings

--- furniture_ecomm/furniture_ecomm/spiders/home_page_spider.py
+++ furniture_ecomm/furniture_ecomm/spiders/home_page_spider.py
@@ -15,7 +15,8 @@ client = ScraperAPIClient(api_key = os.getenv("SCRAPER_API_KEY"))
 custom_settings_dict = {
     "FEED_EXPORT_ENCODING": "utf-8", # UTF-8 deals with all types of characters
     "RETRY_TIMES": 3, # Retry failed requests up to 3 times
-    "AUTOTHROTTLE_ENABLED": False, # Disables the AutoThrottle extension (recommended to be used with proxy services unless the website is tough to crawl)
+    "AUTOTHROTTLE_ENABLED": False, # Disables the AutoThrottle extension (recommended to be used with proxy services)
+    "RANDOMIZE_DOWNLOAD_DELAY": False, # Should not be used with proxy services. If enabled, Scrapy will wait a random amount of time (between 0.5 * DOWNLOAD_DELAY and 1.5 * DOWNLOAD_DELAY) while fetching requests from the same website
     "CONCURRENT_REQUESTS": 5, # The maximum number of concurrent (i.e. simultaneous) requests that will be performed by the Scrapy downloader
     "DOWNLOAD_TIMEOUT": 60 # Setting the timeout parameter to 60 seconds as per the ScraperAPI documentation
 }
